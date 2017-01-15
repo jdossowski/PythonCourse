7e72602a4f0c1abcba9d7c8e4e39c7853ba7bff6
jdossowski <johndean76@gmail.com>
2017-01-15 12:46:33 -0800

    Program to move file based on time last modified
    This Python program moved files that were created or modified less than 24 hours ago.  Results of testing this program were recorded on the following google doc - https://docs.google.com/document/d/12Csyybib6jUQwNz0EtVFdgXinWnZ6WEHf9bR3I9aVAU/edit?usp=sharing

---
+++ Item64.py
@@ -0,0 +1,27 @@
+'''
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18)
+
+Author: John Ossowski, Tech Academy Student
+
+Purpose: Learning to code in Python!  (Item 64 in Python Course)
+This program will move a file from one folder to another based on when they were
+last modified.
+'''
+
+import shutil, os, time
+
+source = 'C:\\Users\Student\Desktop\Folder_A\\' #additional \ needed to escape the \ for the file name string
+destination = 'C:\\Users\Student\Desktop\Folder_B'
+
+files = os.listdir(source)  #generates a list of file names
+
+for file in files:
+    status = os.stat(source+file)   #gets status of file
+    modified = status.st_mtime      #isolating the time that file was last modified
+    now = time.time()               #current time
+    recent = (now - modified)/3600  #get age of file in hours
+    if recent < 24:                 #check to see if file recently modified, within past 24 hours
+        shutil.move(source+file,destination)    #moves files
+        print("File: {} is {} hours old and was moved".format(source+file,recent)) #prints file path and age
+        #prints list of files moved as a check
+
